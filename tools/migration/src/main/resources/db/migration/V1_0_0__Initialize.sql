a6d1a0bd6859f37e1a799173a98ff7f9a8030927
bootware <dev@bootware.org>
2020-11-15 10:08:03 +0900

    Update table definition

--- tools/migration/src/main/resources/db/migration/V1_0_0__Initialize.sql
+++ tools/migration/src/main/resources/db/migration/V1_0_0__Initialize.sql
@@ -1,28 +1,33 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE user_info (
-    id VARCHAR(40) PRIMARY KEY DEFAULT uuid_generate_v4 (),
+CREATE TABLE m_user (
+    user_id VARCHAR(40) NOT NULL DEFAULT uuid_generate_v4(),
     ext_auth_id VARCHAR(40),
-	email VARCHAR(100) UNIQUE,
-	enabled BOOLEAN,
-	password VARCHAR(200),
-	provider VARCHAR(255),
-	username VARCHAR(50) UNIQUE,
-    ${commonColumns}
+    user_name VARCHAR(50),
+    password VARCHAR(200),
+    password_updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    login_failed_times INT DEFAULT 0,
+    unlocked BOOLEAN DEFAULT true,
+	mail_address VARCHAR(100) UNIQUE,
+	enabled BOOLEAN DEFAULT true,
+    ${commonColumns},
+    CONSTRAINT m_user_pk PRIMARY KEY (user_id)
 );
 
-CREATE TABLE authority (
-    id VARCHAR(40) PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    ext_auth_id VARCHAR(40),
-	name VARCHAR(100) UNIQUE,
-	${commonColumns}
-);
-
-CREATE TABLE user_authority (
-	user_id VARCHAR(40) NOT NULL,
-	authority_id VARCHAR(40) NOT NULL,
+CREATE TABLE m_role (
+    role_id VARCHAR(40) NOT NULL DEFAULT uuid_generate_v4(),
+    role_name VARCHAR(100),
 	${commonColumns},
-	CONSTRAINT user_authority_pk PRIMARY KEY (user_id, authority_id),
-	CONSTRAINT user_authority_fk1 FOREIGN KEY (user_id) REFERENCES user_info (id),
-	CONSTRAINT user_authority_fk2 FOREIGN KEY (authority_id) REFERENCES authority (id)
+	CONSTRAINT m_role_pk PRIMARY KEY (role_id)
+);
+
+CREATE SEQUENCE seq_t_user_role_id;
+CREATE TABLE t_user_role (
+	id INT NOT NULL DEFAULT nextval('seq_t_user_role_id'),
+	user_id VARCHAR(40) NOT NULL,
+	role_id VARCHAR(40) NOT NULL,
+	${commonColumns},
+	CONSTRAINT t_user_role_pk PRIMARY KEY (id),
+	CONSTRAINT t_user_role_fk1 FOREIGN KEY (user_id) REFERENCES m_user (user_id),
+	CONSTRAINT t_user_role_fk2 FOREIGN KEY (role_id) REFERENCES m_role (role_id)
 );
--- tools/migration/src/test/resources/db/migration/V2_0_0__TestData.sql
+++ tools/migration/src/test/resources/db/migration/V2_0_0__TestData.sql
@@ -1,13 +1,23 @@
-INSERT INTO authority(id, name) VALUES ('a349521s-8315-61d2-m1ds-731hls7ss89dk', 'ROLE_ADMIN');
-INSERT INTO authority(id, name) VALUES ('b349521s-8315-61d2-m1ds-731hls7ss89dk', 'ROLE_USER');
+-- Admin
+INSERT INTO m_user (user_id, user_name, password, mail_address)
+VALUES ('system', 'Admin', '$2a$10$zgPrRttBhV9k8kI5AOv2P.dVXX6HFdynM.7lXp4RJkLFaywzAxIOy', 'admin@bootware.jp');
 
--- Admin Role
-INSERT INTO user_info(id, email, enabled, password, provider, username)
-VALUES ('d349521s-8315-61d2-m1ds-731hls7ss89dk', 'admin@bootware.jp', 'true', '$2a$10$zgPrRttBhV9k8kI5AOv2P.dVXX6HFdynM.7lXp4RJkLFaywzAxIOy', 'LOCAL', 'admin');
-INSERT INTO user_authority(user_id, authority_id) VALUES ('d349521s-8315-61d2-m1ds-731hls7ss89dk', 'a349521s-8315-61d2-m1ds-731hls7ss89dk');
-INSERT INTO user_authority(user_id, authority_id) VALUES ('d349521s-8315-61d2-m1ds-731hls7ss89dk', 'b349521s-8315-61d2-m1ds-731hls7ss89dk');
+-- User
+INSERT INTO m_user (user_id, user_name, password, unlocked, mail_address)
+VALUES
+('user1', 'User1', '$2a$10$zgPrRttBhV9k8kI5AOv2P.dVXX6HFdynM.7lXp4RJkLFaywzAxIOy', true, 'user1@bootware.jp'),
+('user2', 'User2', '$2a$10$zgPrRttBhV9k8kI5AOv2P.dVXX6HFdynM.7lXp4RJkLFaywzAxIOy', true, 'user2@bootware.jp'),
+('user3', 'User3', '$2a$10$zgPrRttBhV9k8kI5AOv2P.dVXX6HFdynM.7lXp4RJkLFaywzAxIOy', false, 'user3@bootware.jp');
 
--- User Role
-INSERT INTO user_info(id, email, enabled, password, provider, username)
-VALUES ('e349521s-8315-61d2-m1ds-731hls7ss89dk', 'user@bootware.jp', 'true', '$2a$10$zgPrRttBhV9k8kI5AOv2P.dVXX6HFdynM.7lXp4RJkLFaywzAxIOy', 'LOCAL', 'user');
-INSERT INTO user_authority(user_id, authority_id) VALUES ('e349521s-8315-61d2-m1ds-731hls7ss89dk', 'b349521s-8315-61d2-m1ds-731hls7ss89dk');
+-- Role
+INSERT INTO m_role (role_id, role_name) VALUES ('admin', 'ROLE_ADMIN');
+INSERT INTO m_role (role_id, role_name) VALUES ('general', 'ROLE_GENERAL');
+
+-- UserRole
+INSERT INTO t_user_role (user_id, role_id)
+VALUES
+('system', 'admin'),
+('system', 'general'),
+('user1', 'general'),
+('user2', 'general'),
+('user3', 'general');
